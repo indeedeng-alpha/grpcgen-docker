f82304679aa6cd87857cf6581175b224a89f3f3a
James Lamine <jlamine@indeed.com>
2020-01-09 17:37:36 -0800

    Generate typescript type definitions for node clients

--- docker/grpcgen-node/docker-entrypoint.sh
+++ docker/grpcgen-node/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 for proto in "$@"; do
-    grpc_tools_node_protoc -I/src --js_out=import_style=commonjs,binary:/out --grpc_out=/out --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` "$proto"
+    grpc_tools_node_protoc \
+        -I/src \
+        --js_out=import_style=commonjs,binary:/out \
+        --grpc_out=/out \
+        --ts_out="service=grpc-node:/out" \
+        --plugin="protoc-gen-ts=$(which protoc-gen-ts)" \
+        --plugin="protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin)" \
+        "$proto"
 done
